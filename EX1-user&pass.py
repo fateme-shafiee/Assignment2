0b202119a7d1ae4d82f28a7a0773c5fb7685b1b5
fateme-shafiee <110778243+fateme-shafiee@users.noreply.github.com>
2022-08-21 19:49:05 +0430

    Add files via upload

---
+++ EX1-user&pass.py
@@ -0,0 +1,17 @@
+PrimaryUsername = 'shafiee'
+PrimaryPassword = 'shafiee78'
+AttemptCounter = 0 
+
+for i in range (3):
+    username = input ('please enter your Username ; ')
+    password = input ('please enter your Password ; ')
+    if (PrimaryUsername == username) and (PrimaryPassword == password):
+        print ('welcom :)')
+        break
+    elif (PrimaryUsername != username) or (PrimaryPassword != password):
+        if (AttemptCounter == 2):
+            print ('your account has been blocked')
+            break
+        print ('your username or password is incorrect.please try again.')
+
+        AttemptCounter += 1
